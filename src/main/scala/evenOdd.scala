182e8923b3d36dc15ad9cec2538fe1c09035af48
Daisy <singhdmgangina@gmail.com>
2023-05-06 12:28:57 +0530

    fixed

--- src/main/scala/evenOdd.scala
+++ src/main/scala/evenOdd.scala
@@ -2,7 +2,7 @@ package daisy.manmohan.singh.hello
 
 private object evenOdd {
   def main(args: Array[String]): Unit = {
-    for(i<- 1 to 123456789)
+    for(i<- 1 to 123)
     { println(
       if(i%2 == 0 ) i else "Not even means its odd "+i
       )
